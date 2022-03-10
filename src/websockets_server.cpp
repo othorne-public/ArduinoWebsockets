9a9034f4a9faffdd3a4fb87d8eb8c9b20104e221
othorne-public <oliver.thorne@hotmail.com>
2022-03-10 16:52:19 +0000

    Force ocpp protocol header

--- src/websockets_server.cpp
+++ src/websockets_server.cpp
@@ -83,6 +83,7 @@ namespace websockets {
         tcpClient->send("Upgrade: websocket\r\n");
         tcpClient->send("Sec-WebSocket-Version: 13\r\n");
         tcpClient->send("Sec-WebSocket-Accept: " + serverAccept + "\r\n");
+        tcpClient->send("Sec-WebSocket-Protocol: ocpp1.6\r\n");
         tcpClient->send("\r\n");
         
         WebsocketsClient wsClient(tcpClient);
